5f881a13b87670f462679aa89042511a728fc0e0
alplky <alicia.pelkey@gmail.com>
2020-10-19 20:22:55 +0000

    printed out column names in each table for creating classes

--- inspect_hawaii.py
+++ inspect_hawaii.py
@@ -11,12 +11,17 @@ inspector = inspect(engine)
 
 # create variable to inspect table names
 table_names = inspector.get_table_names()
-print(table_names)
+# print(table_names)
 # table names are: 'measurement', 'station'
 
 # create variables to inspect inspect column names in each table
 measurement_column_names = inspector.get_columns("measurement")
 station_column_names = inspector.get_columns("station")
 
-#print out column names for each table
+#print out column names for each table to create classes
+print("Measurement Column Names:")
+pprint(measurement_column_names)
+print("---------------------------")
+print("Station Column Names:")
+pprint(station_column_names)
 
