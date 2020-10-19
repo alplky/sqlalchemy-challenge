7b0e63a9a4830f0e4df84f7a431a14154ae358b5
alplky <alicia.pelkey@gmail.com>
2020-10-19 20:18:24 +0000

    inspecting Hawaii db, created inspector, obtained table names, created variables for columns in each table

---
+++ inspect_hawaii.py
@@ -0,0 +1,22 @@
+# set up and dependencies
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+from pprint import pprint
+
+# create engine from sqlite database
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# create inspector
+inspector = inspect(engine)
+
+# create variable to inspect table names
+table_names = inspector.get_table_names()
+print(table_names)
+# table names are: 'measurement', 'station'
+
+# create variables to inspect inspect column names in each table
+measurement_column_names = inspector.get_columns("measurement")
+station_column_names = inspector.get_columns("station")
+
+#print out column names for each table
+
