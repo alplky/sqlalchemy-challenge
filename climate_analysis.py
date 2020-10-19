81eff4f0cd0c6b516590ead5a1a1eba18034c0ce
alplky <alicia.pelkey@gmail.com>
2020-10-19 20:41:01 +0000

    set up and dependencies

---
+++ climate_analysis.py
@@ -0,0 +1,10 @@
+# set up and dependencies
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.types import Date
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+from pprint import pprint
+
