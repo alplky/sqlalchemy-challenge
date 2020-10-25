ed4fa8ab711e39efe4943f0a15986bccb13c92db
alplky <alicia.pelkey@gmail.com>
2020-10-25 12:35:04 -0500

    converted each class to dict, set up first route for welcome page

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 #connect to database
+app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Resources/hawaii.sqlite"
 
 db = SQLAlchemy(app)
@@ -17,6 +19,17 @@ class Measurement(db.Model):
     prcp = db.Column(db.Float)
     tobs = db.Column(db.Float)
 
+    #convert class to dict
+    def to_dict(self):
+        return {
+            column.name: getattr(self, column.name)
+            if not isinstance(
+                getattr(self, column.name), (datetime.datetime, datetime.date)
+            )
+            else getattr(self, column.name).isoformat()
+            for column in self.__table__.columns
+        }
+
 #create class for station table
 
 class Station(db.Model):
@@ -27,3 +40,32 @@ class Station(db.Model):
     longitude = db.Column(db.Float)
     elevation =  db.Column(db.Float)
 
+    #convert class to dict
+    def to_dict(self):
+        return {
+            column.name: getattr(self, column.name)
+            if not isinstance(
+                getattr(self, column.name), (datetime.datetime, datetime.date)
+            )
+            else getattr(self, column.name).isoformat()
+            for column in self.__table__.columns
+        }
+
+# create specified tables
+db.create_all()
+
+# create home page route
+@app.route("/")
+def main():
+    return (
+        f"Welcome to the Climate App Home Page!<br>"
+        f"Available Routes:<br>"
+        f"/api/v1.0/precipitation<br>"
+        f"/api/v1.0/stations<br>"
+        f"/api/v1.0/tobs<br>"
+        f"/api/v1.0/<start><br>"
+        f"/api/v1.0/<start>/<end><br>"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
